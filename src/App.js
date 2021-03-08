a08b936e524bae51512b83e74a4d8ae34d504a0f
Sanych26 <sshostakivsky@gmail.com>
2021-03-08 18:18:16 +0200

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-let num = prompt('Введіть число в десятичній формі: ');
+let num = prompt("Введіть число в десятичній формі: ");
 
-function toBIN( num ) {
-    let out = '', bit = 1;
-    while( num >= bit ) {
-        out = ( num & bit ? 1 : 0 ) + out;
-        bit <<= 1;
-    }
-    return out || '0';
+function toBIN(num) {
+  let out = "",
+    bit = 1;
+  while (num >= bit) {
+    out = (num & bit ? 1 : 0) + out;
+    bit <<= 1;
+  }
+  return out || "0";
 }
-document.write( '<pre>Число в десятичній формі: ' + num + '\n</pre>' );
-document.write('<pre>Число в двоїчній формі: ' + toBIN(num) + '\n</pre>');
+document.write("<pre>Число в десятичній формі: " + num + "\n</pre>");
+document.write("<pre>Число в двоїчній формі: " + toBIN(num) + "\n</pre>");
 const lgnum = num => {
-  let res = '';
+  let res = "";
   const numStr = num;
-  numStr.startsWith('-') ? (res += '1') : (res += '0');
+  numStr.startsWith("-") ? (res += "1") : (res += "0");
   let n = 0;
   for (let i = 0; i < numStr.length; i++) {
-    if (numStr[i] === '1') n++;
+    if (numStr[i] === "1") n++;
   }
-  res += .${n};
+  res += `.${n}`;
   if (n !== 0) {
-    const ind = numStr.indexOf('.');
+    const ind = numStr.indexOf(".");
     if (ind !== -1) {
       for (let i = 0; i < ind; i++) {
-        if (numStr[i] === '1') res += .${ind - i};
+        if (numStr[i] === "1") res += `.${ind - i}`;
       }
       for (let i = ind + 1; i < numStr.length; i++) {
-        if (numStr[i] === '1') res += .-${i - ind};
+        if (numStr[i] === "1") res += `.-${i - ind}`;
       }
     } else {
       for (let i = 0; i < numStr.length; i++) {
-        if (numStr[i] === '1') res += .${numStr.length - i};
+        if (numStr[i] === "1") res += `.${numStr.length - i}`;
       }
     }
   }
